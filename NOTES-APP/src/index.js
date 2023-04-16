39871c2a3281049fcf5974c2ceeee20ec671cb6f
mateoroca <mateojoserocaclemntis@gmail.com>
2023-04-16 19:11:50 -0300

    add comand start:node index.js

--- NOTES-APP/src/index.js
+++ NOTES-APP/src/index.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const passport = require('passport');
 
-
+require('dotenv').config();
 
 //initializacion 
 const app = express();
